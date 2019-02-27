a0cc197b1cc19b3a622489a2be3c18a6ab5fdb2c
lij416123 <lij416@mcmaster.ca>
2019-02-27 02:18:22 +0000

    testing

--- Project01/README.md
+++ Project01/README.md
@@ -1,6 +1,32 @@
-5.1	
-	make the script interactive by system input
-	input of each integers 1/2/3/4/5/6/7 decides to execute each features
-	(currently only complete feature 1) 
-5.2	
-	use grep to find files with "#TODO", and use > to create/overwrite the file todo.log
+5.1 system input
+        make the script interactive by system input
+        input of each integers 1/2/3/4/5/6  decides to execute each features
+        (i.e. 1 represents the custom feature
+              2 represents feature 5.2
+              6 represents feature 5.6 )
+
+
+5.2 create a TODO Log
+        use grep to find files with "#TODO", and use > to create/overwrite the file todo.log
+
+
+5.3 Compile Error Log
+        use find to find out all haskell and python files,
+        compile them and redirect all files with syntax errors to compile_fail.log file
+
+
+5.4 Merge Log
+	grep the commit message with "merge" or "Merge"
+	and get the first column which are the commit hashes, and redirect the output to merge.log
+
+
+5.5 File Type Count
+	 find the name with extensions of each type of files, 
+	then count the number of these files
+
+
+5.6 Delete Temporary Files
+	use the first command before fist pipt | to list all the untracked files
+	grep the ones with ".tmp" extension
+	use xargs to turn the output to be the arguement of rm
+
--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,10 +1,77 @@
 #!/bin/bash
+
+if [ -e "~/CS1XA3/Project01/logs" ] ; then
+  rm -r ~/CS1XA3/Project01/logs
+else
+  mkdir -p ~/CS1XA3/Project01/logs
+  touch ~/CS1XA3/Project01/logs/todo.log ~/CS1XA3/Project01/logs/compile_fail.log ~/CS1XA3/Project01/logs/merge.log 
+fi
+#create a new directory logs containing todo.log compile_fail.log merge.log
+
+
 cd ..
+# going back to ~/CS1XA3 dir
+
 
 if [ $# -eq 0 ] ; then
   echo "No inputs!"
+# check for no input
+
+
 else
-  if [ $@ == 1 ] ; then
+  if [ $@ == 2 ] ; then
     grep -r --exclude="todo.log" "#TODO" > ./Project01/logs/todo.log
   fi
+# 5.2 Create a TODO Log
+# use grep to find files with "#TODO", and use > to create/overwrite the file todo.log
+
+
+  if [ $@ == 3 ] ; then
+    for i in $(find . -name "*.hs") ; do
+      ghc $i 2>> ./Project01/logs/compile_fail.log
+    done
+    for m in $(find . -name "*.py") ; do
+      python $m 2>> ./Project01/logs/compile_fail.log
+    done
+  fi
+# 5.3 compile error log
+# use find to find out all haskell and python files,
+# compile them and redirect all files with syntax errors to compile_fail.log file
+
+
+  if [ $@ == 4 ] ; then
+    git log --oneline | grep -i "merge" | cut -d' ' -f1 > ./Project01/logs/merge.log
+  fi
+# 5.4 Merge Log
+# grep the commit message with "merge" or "Merge" 
+# and get the first column which are the commit hashes, and redirect the output to merge.log
+
+
+  if [ $@ == 5 ] ; then
+    html=$(find . -name "*.html" | wc -l)
+    js=$(find . -name "*.js" | wc -l)
+    css=$(find . -name "*.css" | wc -l)
+    py=$(find . -name "*py" | wc -l)
+    hs=$(find . -name "*.hs" | wc -l)
+    sh=$(find . -name "*.sh" | wc -l)
+    echo "HTML: $html"
+    echo "Javascript: $js"
+    echo "CSS: $css"
+    echo "Python: $py"
+    echo "Haskell： $hs"
+    echo "Bash Script file: $sh"
+  fi
+# 5.5 File Type Count
+# find the name with extensions of each type of files, then count the number of these files
+
+
+  if [ $@ == 6 ] ; then
+    git ls-files . --exclude-standard --others | grep ".tmp" | xargs rm
+  fi
+# 5.6 Delete Temporary Files
+# use the first command before fist pipt | to list all the untracked files
+# grep the ones with ".tmp" extension
+# use xargs to turn the output to be the arguement of rm
+
+
 fi
