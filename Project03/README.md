597d98f26b77cdc0ea5eff4a83da6772cd4b4c3f
lij416123 <lij416@mcmaster.ca>
2019-04-27 10:58:09 +0000

    project03

---
+++ Project03/README.md
@@ -0,0 +1,1 @@
+A Tetris Game with pause, timer, score board and the hint of next shape, and restart functrion. 
---
+++ Project03/django_project/login/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+class User(models.Model):
+    name = models.CharField(unique=True)
+    password = models.CharField
+    c_time = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['c_time']
+# Create your models here.
